09525870cd33d7138ba17d4069caffce269ccbdb
Ben <agentflippy5@gmail.com>
2025-04-16 18:28:21 -0400

    0.4.0 Release: added web-based GUI

--- sharkphin.ps1
+++ sharkphin.ps1
@@ -14,7 +14,62 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-$version = "0.3.1"
+#Function definitions
+
+#Create new search in 365
+function New-CustomSearch {
+    param (
+        [string]$ContentSearchName,
+        [string]$InputPhishSenderName,
+        [string]$InputPhishSubject
+    )
+
+    #make "" into '' because KQL is weird
+    $InputPhishSubject = $InputPhishSubject.Replace('"', "'")
+    #Escape quotes for KQL query string
+    $InputPhishSubject = $InputPhishSubject.Replace("'","''")
+
+	#Combine the email address and the subject into one string
+    $InputMatchQuery="(From:"+$InputPhishSenderName+") AND (Subject:`""+$InputPhishSubject+"`")"
+
+    Write-Host "Building search $ContentSearchName..."
+
+    #Create a Content Search to find the message
+    try{
+        $Search=New-ComplianceSearch -Name $ContentSearchName -ExchangeLocation All -ContentMatchQuery $InputMatchQuery
+    } catch {
+        #If the search fails, cut back to the main menu.
+        Write-Host "There was an error creating the content search in 365. The search query generated was:"
+        Write-Host $InputMatchQuery
+        Write-Host "Please consider reporting an issue at https://github.com/ArtezzaIT/Sharkphin/issues"
+        return "There was an error creating the content search in 365. The search query generated was: $InputMatchQuery"
+    }
+
+    #Start Content Search
+    Start-ComplianceSearch -Identity $Search.Identity
+
+    #ADD running search message
+    Write-Host "Content Search is running. Please check the Sharkphin or Purview portal for status."
+    return "The content search ##SEARCHNAME## was successfully submitted as ##UPN##. Please check the Sharkphin or Purview portal for status."
+}
+
+# Function to parse query string into a hashtable
+function Parse-QueryString {
+    param (
+        [string]$queryString
+    )
+    $queryParams = @{}
+    if ($queryString) {
+        $queryString -split "&" | ForEach-Object {
+            $paramParts = $_ -split "="
+            $queryParams[$paramParts[0]] = [System.Web.HttpUtility]::UrlDecode($paramParts[1])
+        }
+    }
+    return $queryParams
+}
+
+#Program initialization
+$version = "0.4.0"
 $channel = "stable"
 
 write-host "        ______     ______     ______   ______     ______     ______     ______                "
@@ -33,15 +88,31 @@ write-host "                                                                    
 Write-Host "Written by Benjamin and Madeline"
 Write-Host "Version $version, $channel channel"
 
+#Check platform
+if ($PSVersionTable.PSVersion.Major -lt 6) {
+    # Code to run if PowerShell version is below 6
+    Write-Host "Running on PowerShell version below 6"
+    $IsWindows = $true
+    $IsMacOS = $false
+    $IsLinux = $false
+}
+
+
 #Check if running the latest/recommended version
 try {
-    $availableonline = (invoke-webrequest -uri https://sharkphin.artezza.io/latest.json -usebasicparsing).content | ConvertFrom-Json
+    $availableonline = (Invoke-WebRequest -uri https://sharkphin.artezza.io/latest.json -usebasicparsing).content | ConvertFrom-Json
     if ($availableonline.$channel[0] -ne $version){
         Write-host "You are not running the recommended version for the $channel channel!"
         $updatechoice = Read-Host "Would you like to download the recommended verion? (y/N)"
         if ($updatechoice.ToLower() -eq "y"){
             $downloadlnk = $availableonline.$channel[1]
-            cmd /c "START $downloadlnk"
+            if ($IsWindows) {
+                Start-Process $downloadlnk
+            } elseif ($IsMacOS) {
+                Start-Process "open" -ArgumentList $downloadlnk
+            } elseif ($IsLinux) {
+                Start-Process "xdg-open" -ArgumentList $downloadlnk
+            }
             exit
         }
     } else {
@@ -53,6 +124,7 @@ catch {
 }
 
 #Setup dependancies
+Write-Host "Setting up modules..."
 try {
     #Install EXO module if missing
     Install-Module -Name ExchangeOnlineManagement
@@ -64,12 +136,13 @@ catch {
     Write-Output "Attempting to continue without updating..."
 }
 
-
 #Connect to Security & Compliance PowerShell, then Exchange Online
+Write-Host "Connecting to 365..."
 try{
     Import-Module ExchangeOnlineManagement
     Connect-IPPSSession -ShowBanner:$false
-    Connect-ExchangeOnline -ShowBanner:$false -UserPrincipalName (Get-ConnectionInformation).UserPrincipalName
+    $UPN = (Get-ConnectionInformation).UserPrincipalName
+    Connect-ExchangeOnline -ShowBanner:$false -UserPrincipalName $UPN
 }
 catch {
     Write-Host "There was an error signing into the 365 tenant. Please verify the credentials you are using are correct and that the Exchange Online Module is installed."
@@ -78,147 +151,196 @@ catch {
     exit
 }
 
-function CheckSearchStatus {
-    #Function to wait for search to complete
-    param (
-        $SearchName
-    )
-    #FGet search and status
-    $SearchStatus = Get-ComplianceSearch -Identity $SearchName | Select-Object -Property Status
-    if ($SearchStatus.Status -ne "Completed"){
-        #if not complete, print status
-        Write-Host "Search not yet complete... Current status:", $SearchStatus.Status
-        #wait 15 seconds
-        Start-Sleep -Seconds 15
-        #do it again
-        CheckSearchStatus $SearchName
-    }
-    else {
-        Write-Host "Search complete!"
-    }
+#Server setup
+Write-Host "Starting server..."
+$port = 8080
+$listener = [System.Net.Sockets.TcpListener]::new([System.Net.IPAddress]::Loopback, $port)
+$listener.Start()
+$portal = "http://localhost:$port/"
+Write-Host "Server running on $portal/"
+if ($IsWindows) {
+    Start-Process $portal
+} elseif ($IsMacOS) {
+    Start-Process "open" -ArgumentList $portal
+} elseif ($IsLinux) {
+    Start-Process "xdg-open" -ArgumentList $portal
 }
-function Get-SearchFromUser {
 
-    #Prompt user to enter parameters for content search
-    $ContentSearchName=Read-Host -Prompt "Enter Unique Content Search Name"
-    $InputPhishSenderName=Read-Host -Prompt "Enter the full email address of the phisher"
-    $InputPhishSubject=Read-Host -Prompt "Enter the exact subject of the phish"
 
-    #make "" into '' because KQL is weird
-    $InputPhishSubject = $InputPhishSubject.Replace('"', "'")
-    #Escape quotes for KQL query string
-    $InputPhishSubject = $InputPhishSubject.Replace("'","''")
+#Request loop
+$activeSession = $true
+while ($activeSession) {
+    #Accept request
+    $client = $listener.AcceptTcpClient()
+    $stream = $client.GetStream()
+    $reader = New-Object System.IO.StreamReader($stream)
+    $request = $reader.ReadLine()
 
-	#Combine the email address and the subject into one string
-    $InputMatchQuery="(From:"+$InputPhishSenderName+") AND (Subject:`""+$InputPhishSubject+"`")"
+    Write-Host "Received request: $request"
 
-    Write-Host "Building search..."
+    # Parse the request to get the requested page and query parameters
+    $pageRequest = $request -split " " | Select-Object -Index 1
+    $pageRequestParts = $pageRequest -split "\?"
+    $pagePath = $pageRequestParts[0]
+    $queryString = if ($pageRequestParts.Count -gt 1) { $pageRequestParts[1] } else { "" }
+    $queryParams = Parse-QueryString -queryString $queryString
 
-    #Create a Content Search to find the message
-    try{
-        $Search=New-ComplianceSearch -Name $ContentSearchName -ExchangeLocation All -ContentMatchQuery $InputMatchQuery
-    } catch {
-        #If the search fails, cut back to the main menu.
-        Write-Host "There was an error creating the content search in 365. The search query generated was:"
-        Write-Host $InputMatchQuery
-        Write-Host "Please consider reporting an issue at https://github.com/ArtezzaIT/Sharkphin/issues"
-        Write-Host "You are now being returned to the main menu..."
-        main_menu
-    }
+    Write-Host $queryParams
 
-    #Start Content Search
-    Start-ComplianceSearch -Identity $Search.Identity
-
-    #ADD running search message
-    Write-Output "Content Search is running. Please wait..."
-
-    #Wait to continue to allow Content Search to finish
-    CheckSearchStatus $ContentSearchName
-
-    #Return the ContentSearchName variable for the soft delete loop below 
-    Set-Variable -Name "ContentSearchName" -Value $ContentSearchName -scope global
-    Set-Variable -Name "PhishSenderName" -Value $InputPhishSenderName -scope global
-
-    #System message to tell them to review number before deleting 
-    Write-Output "Review number of items found in Content Search. Items should be greater than 0."
-
-    #Confirm number of items found from the search
-    Get-ComplianceSearch -Identity $ContentSearchName | Select-Object -Property Name, Items | Format-Table -AutoSize
-
-    
-}
-function SearchAndPurge {
-    Get-SearchFromUser
-    #SOFT DELETE SECTION
-    #Loop for user input to confirm deleting
-    do {
-        $response = Read-Host -Prompt "Are you ready to remove these items from all inboxes? (Y/N)"
-        $response = $response.ToLower()
-        if ($response -eq 'y') {
-            New-ComplianceSearchAction -SearchName $ContentSearchName -Purge -PurgeType SoftDelete
-            break
+    switch ($pagePath) {
+        "/" {
+            # Default page, do nothing special
+            $htmlFilePath = "$PSScriptRoot\app.html"
+            $htmlContent = Get-Content -Path $htmlFilePath -Raw
+            $htmlContent =  $htmlContent -replace "##UPN##", $UPN
+            $response = "HTTP/1.1 200 OK`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
         }
-    } until (
-        ($response -eq 'n')
-    )
-    BlockThem $PhishSenderName
-}
-function BlockThem {
-    param (
-        $email
-    )
-    #get phish domain
-    $phishdomain = $email.substring($email.indexof("@")+1)
-    #Loop asking to block domain/email
-    do {
-        $response = Read-Host -Prompt "Would you like to mark the entire $phishdomain domain as spam in this tenant? (Y/N)"
-        $response = $response.ToLower()
-        if ($response -eq 'y') {
-            New-TenantAllowBlockListItems -ListType Sender -Block -Entries $phishdomain -NoExpiration
-            break
+        "/favicon.ico" {
+            # Handle favicon request
+            $faviconPath = "$PSScriptRoot/favicon.ico"
+            $faviconContent = [System.IO.File]::ReadAllBytes($faviconPath)
+            $response = "HTTP/1.1 200 OK`r`nContent-Type: image/x-icon`r`n`r`n"
+            $buffer = [System.Text.Encoding]::ASCII.GetBytes($response) + $faviconContent
         }
-        else{
-            #Loop asking to block domain/email
-            do {
-                $response = Read-Host -Prompt "Would you like to mark the specific email address $email as spam within this tenant? (Y/N)"
-                $response = $response.ToLower()
-                if ($response -eq 'y') {
-                    New-TenantAllowBlockListItems -ListType Sender -Block -Entries $email -NoExpiration
-                    break
-                }
-            } until (
-                ($response -eq 'n')
-            )
-            break
-                }
-    } until (
-        ($response -eq 'n')
-    )
-}
-function main_menu {
-    Write-Host "========================="
-    Write-Host "1. Search and Purge mail"
-    Write-Host "2. Block email and/or domain"
-    Write-Host "3. Exit"
-    $selection = Read-Host "Select a number from the menu"
-    if ($selection -eq "1"){
-        SearchAndPurge
-        main_menu
-    }elseif ($selection -eq "2") {
-        $email = Read-Host "Enter an email address"
-        BlockThem $email
-        main_menu
-    }elseif ($selection -eq "3"){
-        #Disconnect from session 
-        Disconnect-ExchangeOnline -Confirm:$false
+        "/background.jpg" {
+            # Handle background request
+            $backgroundPath = "$PSScriptRoot/background.jpg"
+            $backgroundContent = [System.IO.File]::ReadAllBytes($backgroundPath)
+            $response = "HTTP/1.1 200 OK`r`nContent-Type: image/jpg`r`n`r`n"
+            $buffer = [System.Text.Encoding]::ASCII.GetBytes($response) + $backgroundContent
+        }
+        "/logout" {
+            # Handle logout page request
+            Write-Host "Handling logout request"
+            $htmlFilePath = "$PSScriptRoot\logout.html"
+            $activeSession = $false
+            $htmlContent = Get-Content -Path $htmlFilePath -Raw
+            $response = "HTTP/1.1 200 OK`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+        }
+        "/search" {
+            # Handle search page request
+            Write-Host "Handling search request"
+            # Extract search query parameters
+            $searchName = $queryParams["name"]
+            $phishSenderName = $queryParams["sender"]
+            $phishSubject = $queryParams["subject"]
 
-        #Message: Disconnected successfully, you can close window
-        Read-Host "Session disconnected successfully. Hit enter to close"
-        exit
-    }else {
-        Write-Host "Sorry! That option wasn't understood. Please try again."
-        main_menu
+            # Perform search operation
+            if ($searchName -and $phishSenderName -and $phishSubject) {
+                $resultMessage = New-CustomSearch -ContentSearchName $searchName -InputPhishSenderName $phishSenderName -InputPhishSubject $phishSubject
+                $htmlFilePath = "$PSScriptRoot\search.html"
+                $htmlContent = Get-Content -Path $htmlFilePath -Raw
+                $htmlContent =  $htmlContent -replace "##RESULTMESSAGE##", $resultMessage
+                $htmlContent =  $htmlContent -replace "##UPN##", $UPN
+                $htmlContent =  $htmlContent -replace "##SEARCHNAME##", $searchName
+                $response = "HTTP/1.1 200 OK`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            } else {
+                $htmlFilePath = "$PSScriptRoot\400.html"
+                $htmlContent = Get-Content -Path $htmlFilePath -Raw
+                $response = "HTTP/1.1 400 Bad Request`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            }
+        }
+        "/searchstatus" {
+            # Return JSON string of compliance search information
+            Write-Host "Handling search status request"
+            $searches = @()
+            $availableSearches = Get-ComplianceSearch
+            foreach ($search in $availableSearches){
+                $searchData = Get-ComplianceSearch -Identity $search.Identity | Select-Object -Property Name, Items, Status
+                $searches += $searchData
+            }
+            if ($searches.Count -eq 0) {
+                $searches = @{"Message" = "No searches found."}
+            }
+            # Convert the search data to JSON
+            $searchesJson = $searches | ConvertTo-Json
+            $response = "HTTP/1.1 200 OK`r`nContent-Type: application/json`r`n`r`n$searchesJson"
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+        }
+        "/delete" {
+            # Delete a compliance search
+            Write-Host "Handling delete search request"
+            $searchName = $queryParams["name"]
+            if ($searchName) {
+                try {
+                    Remove-ComplianceSearch -Identity $searchName -Confirm:$false
+                    $answer = "Search $searchName deleted successfully"
+                }
+                catch {
+                    $answer = "Error deleting search $searchName"
+                }
+                $response = "HTTP/1.1 200 OK`r`nContent-Type: text/plain`r`n`r`n$answer"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            } else {
+                $htmlFilePath = "$PSScriptRoot\400.html"
+                $htmlContent = Get-Content -Path $htmlFilePath -Raw
+                $response = "HTTP/1.1 400 Bad Request`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            }
+        }
+        "/purge" {
+            # Delete a compliance search
+            Write-Host "Handling purge search request"
+            $searchName = $queryParams["name"]
+            if ($searchName) {
+                try {
+                    New-ComplianceSearchAction -SearchName $searchName -Purge -PurgeType SoftDelete -Confirm:$false
+                    $answer = "Search $searchName purging successfully"
+                }
+                catch {
+                    $answer = "Error purging search $searchName"
+                }
+                $response = "HTTP/1.1 200 OK`r`nContent-Type: text/plain`r`n`r`n$answer"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            } else {
+                $htmlFilePath = "$PSScriptRoot\400.html"
+                $htmlContent = Get-Content -Path $htmlFilePath -Raw
+                $response = "HTTP/1.1 400 Bad Request`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            }
+        }
+        "/restart" {
+            # Restart a compliance search
+            Write-Host "Handling restart search request"
+            $searchName = $queryParams["name"]
+            if ($searchName) {
+                try {
+                    Start-ComplianceSearch -Identity $searchName
+                    $answer = "Search $searchName restarted successfully"
+                }
+                catch {
+                    $answer = "Error restarting search $searchName"
+                }
+                $response = "HTTP/1.1 200 OK`r`nContent-Type: text/plain`r`n`r`n$answer"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            } else {
+                $htmlFilePath = "$PSScriptRoot\400.html"
+                $htmlContent = Get-Content -Path $htmlFilePath -Raw
+                $response = "HTTP/1.1 400 Bad Request`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+            }
+        }
+        default {
+            # Handle unknown page request
+            Write-Host "Unknown page request: $pageRequest"
+            $htmlFilePath = "$PSScriptRoot\404.html"
+            $htmlContent = Get-Content -Path $htmlFilePath -Raw
+            $response = "HTTP/1.1 200 OK`r`nContent-Type: text/html`r`n`r`n$htmlContent"
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($response)
+        }
     }
+
+    $stream.Write($buffer, 0, $buffer.Length)
+    $stream.Close()
+    $client.Close()
 }
 
-main_menu
+#Cleanup
+Write-Host "Logging out and ending processes..."
+$listener.Stop()
+Disconnect-ExchangeOnline -Confirm:$false
+Read-Host "Session disconnected successfully. Hit enter to close"
